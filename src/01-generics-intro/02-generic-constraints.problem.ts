94786f107536ceff54b931c52e8322b7e5383bb9
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-19 11:25:45 +0200

    02

--- src/01-generics-intro/02-generic-constraints.problem.ts
+++ src/01-generics-intro/02-generic-constraints.problem.ts
@@ -1,10 +1,14 @@
 import { it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-export const returnWhatIPassIn = <T>(t: T) => t;
+export const returnWhatIPassIn = <StringType extends string>(
+  t: StringType
+): StringType => t;
 
 it("Should ONLY allow strings to be passed in", () => {
   const a = returnWhatIPassIn("a");
+
+  // generic must be of a certain type
 
   type test1 = Expect<Equal<typeof a, "a">>;
 
