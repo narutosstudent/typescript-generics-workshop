fdadb982a67ed30782283624deded41e36ddd6a5
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-25 08:54:29 +0200

    15

--- src/03-art-of-type-arguments/15-array-or-array-member-in-generics.problem.ts
+++ src/03-art-of-type-arguments/15-array-or-array-member-in-generics.problem.ts
@@ -1,11 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const makeStatus = <TStatuses extends string[]>(statuses: TStatuses) => {
+const makeStatus = <TStatuse extends string>(statuses: TStatuse[]) => {
   return statuses;
 };
 
 const statuses = makeStatus(["INFO", "DEBUG", "ERROR", "WARNING"]);
 
 type tests = [
-  Expect<Equal<typeof statuses, Array<"INFO" | "DEBUG" | "ERROR" | "WARNING">>>,
+  Expect<Equal<typeof statuses, Array<"INFO" | "DEBUG" | "ERROR" | "WARNING">>>
 ];
