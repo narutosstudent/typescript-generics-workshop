84d776ac7655a5e6b271f1343cfba59d33aece8c
Tiger Abrodi <tigerabrodi@gmail.com>
2023-08-26 17:47:10 +0200

    29

--- src/06-challenges/29-infinite-scroll.problem.ts
+++ src/06-challenges/29-infinite-scroll.problem.ts
@@ -1,16 +1,42 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const makeInfiniteScroll = (params: unknown) => {
-  const data = params.initialRows || [];
+// Define a type for a row which has a required key (of type Key) and allows for any other properties.
+type RowWithKey<Key extends string> = { [P in Key]: unknown } & Record<
+  string,
+  unknown
+>;
 
+// Define the `makeInfiniteScroll` function with a generic parameter Key that extends string.
+const makeInfiniteScroll = <Key extends string>(params: {
+  // Ensure the given key is of type Key.
+  key: Key;
+  // A function that fetches rows; these rows should have the required key.
+  fetchRows: () => Promise<RowWithKey<Key>[]>;
+  // An optional initial set of rows. They too should have the required key.
+  initialRows?: RowWithKey<Key>[];
+}): {
+  // A function to trigger the fetch of more rows.
+  scroll: () => Promise<void>;
+  // A function to retrieve all fetched rows.
+  getRows: () => RowWithKey<Key>[];
+} => {
+  // Define a mutable local array data to store fetched rows. Initialize with initialRows or an empty array.
+  let data: RowWithKey<Key>[] = params.initialRows || [];
+
+  // Define the scroll function, which fetches more rows and appends them to the data array.
   const scroll = async () => {
+    // Fetch more rows using the provided fetchRows function.
     const rows = await params.fetchRows();
-    data.push(...rows);
+    // Append the fetched rows to the existing data array.
+    data = [...data, ...rows];
   };
 
+  // Return an object with the scroll function and the getRows function.
   return {
+    // The scroll function fetches and appends more rows.
     scroll,
+    // The getRows function simply returns the data array.
     getRows: () => data,
   };
 };
@@ -33,9 +59,9 @@ it("Should fetch more data when scrolling", async () => {
 
 it("Should ensure that the key is one of the properties of the row", () => {
   makeInfiniteScroll({
-    // @ts-expect-error
     key: "name",
     fetchRows: () =>
+      // @ts-expect-error
       Promise.resolve([
         {
           id: "1",
